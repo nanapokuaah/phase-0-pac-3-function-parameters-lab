f851e7328e626055958ba361b7b5a906467f3a8e
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-09-21 18:45:43 -0400

    commit tree f865ef65874269db11dff1891a3793225331a255

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction (name){
+    name === `Aki`
+    console.log (name)
+    return(`Hi, my name is ${name}.`)
+}
+
+function introductionWithLanguage(name,language){
+    name === `Aki`
+    language === `Ember.js`
+    console.log (name,language)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+function introductionWithLanguageOptional(name,language){
+    name ===`Gracie`
+    console.log (name, language)
+}else if (language = `JavaScript`){
+}
+    return (`Hi, my name is ${`Gracie`} and I am learning to program in ${`JavaScript`}.`)
+}
